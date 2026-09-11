dae84b332d44c12c16fca2140e985b863fb69837
leyon <3039613979@qq.com>
2026-09-11 12:54:06 +0800

    初始化项目

---
+++ AuraVision-backend/src/main/java/com/leyon/auravision/AuraVisionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.leyon.auravision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuraVisionBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuraVisionBackendApplication.class, args);
+    }
+
+}
